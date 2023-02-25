56513e3cebeed76b8883f8537d0843b17dc9a807
Rinku Jat <100854107+Rinku9172@users.noreply.github.com>
2023-02-25 21:29:44 +0530

    Update main.dart

--- Screens/main.dart
+++ Screens/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:assignments/Addition.dart';
+import 'package:assignments/Checkbox.dart';
 import 'package:flutter_application_1/Screens/Screen1.dart';
 import 'package:flutter_application_1/Screens/Screen2.dart';
 import 'package:flutter_application_1/Screens/Screen3.dart';
-
 import 'package:flutter_application_1/Screens/Screen4.dart';
 import 'package:flutter_application_1/Screens/Screen5.dart';
-
+import 'package:assignments/TextFieldWidget.dart';
+import 'package:assignments/RadioButton.dart';
 void main() {
   runApp(s1());
 }
